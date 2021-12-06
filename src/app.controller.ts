2b6ac1db91423e7547b998d82fd787748d9871f2
empoweron <95541132+empoweron@users.noreply.github.com>
2021-12-06 21:28:56 +0200

    lab2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,25 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+
+  getNumber(): number{
+    return this.appService.getNumber();
   }
+  
+  getArrays(): number[]{
+    return this.appService.getArrays();
+  }
+  
+  getEnums(): number{
+    return this.appService.getEnums();
+  }
+  
+  getMultitypeValues():number|string{
+    return this.appService.getMultitypeValues();
+  }
+  
+  getMultitupeArrays(): (number|string)[][]{
+    return this.appService.getMultitypeArrays();
+  }
+  
 }
